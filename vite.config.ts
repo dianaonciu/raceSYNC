f8e1d4cb7af251590db5c9ebb2af1bef9493a2bb
LeoSiegmund <leonhard.siegmund@online.de>
2025-06-19 15:21:39 +0200

    test 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/raceSYNC/', // Important: Set this to your GitHub repo name
+  base: '/raceSYNC/',
 })
